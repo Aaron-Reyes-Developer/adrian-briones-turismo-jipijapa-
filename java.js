c36057724ca78d2353034264211e790b5e949066
Aaron-Reyes-Developer <92542964+Aaron-Reyes-Developer@users.noreply.github.com>
2022-01-06 16:51:38 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+function Enlace_1 (){
+   location.href = "https://goo.gl/maps/6UJFobPZUCMeb1ku5"; 
+}
+
+function Enlace_2 (){
+    location.href = "https://goo.gl/maps/JLPgQFvo7aWMF9m9A"; 
+}
+
+function Enlace_3 (){
+    location.href = "https://goo.gl/maps/JTXC8ZNdVCFENtZD7"; 
+}
+
+function Enlace_4 (){
+    location.href = "https://goo.gl/maps/zrHc6ZNhNzHCJgSa7"; 
+}
+
+
+function comida_ceviche(){
+    location.href = "https://www.google.com/search?client=opera-gx&q=que+es+el+ceviche+ecuatoriano&sourceid=opera&ie=UTF-8&oe=UTF-8"; 
+}
+
+function comida_humita(){
+    location.href = "https://www.google.com/search?q=que+es+la+humita+ecuatoriano&client=opera-gx&hs=agD&ei=FQjXYb-RIYeIwbkP3OSowAs&ved=0ahUKEwj_7-r4tZ31AhUHRDABHVwyCrgQ4dUDCA0&uact=5&oq=que+es+la+humita+ecuatoriano&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEM0CMgUIABDNAjIFCAAQzQI6BwgAEEcQsAM6BggAEAcQHjoICAAQCBAHEB46BggAEA0QHjoICAAQCBANEB46CggAEAgQDRAKEB5KBAhBGABKBAhGGABQ_xFY1SVgjSdoBnACeACAAZkCiAGpD5IBBjAuMTMuMZgBAKABAcgBCMABAQ&sclient=gws-wiz";
+}
+
+function comida_bollo(){
+    location.href = "https://spiegato.com/es/que-es-un-bollo-de-cerdo";
+}
